f73c911766b402f732a5c5db827439739ff722f4
Alexey Gerasev <alexey.gerasev@gmail.com>
2022-10-30 14:16:32 +0700

    Safe context attachment

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,19 @@ mod sys;
 pub mod error;
 pub mod types;
 
-use error::Error;
+use crate::{error::Error, types::DbRequest};
+use std::{
+    ffi::{self, CStr},
+    marker::PhantomData,
+    ptr::{self, NonNull},
+    rc::Rc,
+    time::Duration,
+};
 
 #[derive(Debug)]
 pub struct Context {
-    raw: *mut sys::ca_client_context,
+    raw: NonNull<sys::ca_client_context>,
 }
-
 impl Context {
     pub fn new(preemptive: bool) -> Result<Self, Error> {
         let select = if preemptive {
@@ -18,26 +24,82 @@ impl Context {
             sys::ca_preemptive_callback_select::ca_disable_preemptive_callback
         };
         Error::try_from_raw(unsafe { sys::ca_context_create(select) })?;
-        let raw = unsafe { sys::ca_current_context() };
-        assert!(!raw.is_null());
-        Self::detach();
-        Ok(Self { raw })
+        let raw = Self::current();
+        unsafe { Self::detach() };
+        Ok(Self {
+            raw: NonNull::new(raw).unwrap(),
+        })
     }
-    fn attach(&mut self) {
-        unsafe { sys::ca_attach_context(self.raw) };
+    fn current() -> *mut sys::ca_client_context {
+        unsafe { sys::ca_current_context() }
     }
-    fn detach() {
-        unsafe { sys::ca_detach_context() };
+    unsafe fn attach_unchecked(&self) {
+        sys::ca_attach_context(self.raw.as_ptr());
+    }
+    unsafe fn detach() {
+        sys::ca_detach_context();
+    }
+    pub fn attach(&self) -> AttachGuard<'_> {
+        AttachGuard::new(self)
     }
 }
 impl Drop for Context {
     fn drop(&mut self) {
-        self.attach();
-        unsafe { sys::ca_context_destroy() };
+        unsafe {
+            self.attach_unchecked();
+            sys::ca_context_destroy();
+        }
     }
+}
+pub struct AttachGuard<'a> {
+    owner: PhantomData<&'a Context>,
+}
+impl<'a> AttachGuard<'a> {
+    fn new(owner: &'a Context) -> Self {
+        assert!(Context::current().is_null());
+        unsafe { owner.attach_unchecked() };
+        Self { owner: PhantomData }
+    }
+}
+impl<'a> Drop for AttachGuard<'a> {
+    fn drop(&mut self) {
+        unsafe { Context::detach() };
+    }
+}
+
+fn pend_io(ctx: &Context, timeout: Duration) -> Result<(), Error> {
+    let _guard = ctx.attach();
+    Error::try_from_raw(unsafe { sys::ca_pend_io(timeout.as_secs_f64()) })
 }
 
 #[derive(Debug)]
 pub struct Channel {
-    raw: sys::chanId,
+    ctx: Rc<Context>,
+    raw: NonNull<ffi::c_void>,
+}
+impl Channel {
+    pub fn new(ctx: Rc<Context>, name: &CStr) -> Result<Self, Error> {
+        let mut raw: sys::chanId = ptr::null_mut();
+        {
+            let _guard = ctx.attach();
+            Error::try_from_raw(unsafe {
+                sys::ca_create_channel(name.as_ptr(), None, ptr::null_mut(), 0, &mut raw as *mut _)
+            })?;
+        }
+        pend_io(&ctx, Duration::ZERO)?;
+        let raw = NonNull::new(raw).unwrap();
+        Ok(Self { ctx, raw })
+    }
+    pub fn context(&self) -> &Rc<Context> {
+        &self.ctx
+    }
+    fn raw(&self) -> sys::chanId {
+        self.raw.as_ptr()
+    }
+}
+impl Drop for Channel {
+    fn drop(&mut self) {
+        let _guard = self.ctx.attach();
+        unsafe { sys::ca_clear_channel(self.raw()) };
+    }
 }
